37cebef615ab21f028c35e06e80591c54cd8f2d9
MDViktor <milliondollarboymvd@gmail.com>
2022-08-24 00:44:52 +0300

    init: difficulty choise

--- data/script.js
+++ data/script.js
@@ -1,15 +1,44 @@
 // import ancientsData from "./ancients.js";
 
 const cards = document.querySelectorAll('.ancient_card');
+const difficulty = document.querySelectorAll('.difficulty');
+const secondPhase = document.querySelector('.second_phase');
+const thirdPhase = document.querySelector('.third_phase');
+const shuffle = document.querySelector('.shuffle');
 
 cards.forEach(element => {
   element.addEventListener('click', ()=>{
-    if (element.classList.contains('active')){
-      element.classList.remove('active');
-    } else {
+    if (!element.classList.contains('active')){
       cards.forEach( n=>n.classList.remove('active'));
       element.classList.add('active');
-    }
+    } 
     
   })
 });
+function difficultyChoise () {
+  difficulty.forEach(element => {
+    element.addEventListener('click', ()=>{
+      if (!element.classList.contains('active')){
+        difficulty.forEach( n=>n.classList.remove('active'));
+        element.classList.add('active');
+        thirdPhase.style.display = 'block';
+      } 
+      
+    })
+  });
+  
+}
+function getDifficulty() {
+  cards.forEach(element => {
+    if (element.classList.contains('active')){
+      secondPhase.style.display = 'block';
+      difficultyChoise();
+    }
+  });
+  setTimeout(getDifficulty, 500)
+}
+
+getDifficulty()
+shuffle.addEventListener('click', ()=>{
+  shuffle.style.display = 'none';
+})
