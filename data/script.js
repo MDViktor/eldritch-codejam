11c104a982150dece2c10393d3247f0d362493b1
MDViktor <milliondollarboymvd@gmail.com>
2022-08-25 00:07:56 +0300

    feat: tryout shuffle

--- data/script.js
+++ data/script.js
@@ -7,7 +7,7 @@ const thirdPhase = document.querySelector('.third_phase');
 const shuffle = document.querySelector('.shuffle');
 const deck = document.querySelector('.deck');
 const lastCard = document.querySelector('.last_card');
-let selectedCardPath = '';
+var selectedCardIndex;
 
 cards.forEach(element => {
   element.addEventListener('click', ()=>{
@@ -15,7 +15,13 @@ cards.forEach(element => {
       cards.forEach( n=>n.classList.remove('active'));
       element.classList.add('active');
     } 
-    
+    for(let i=0;i<ancientsData.length;i++){
+      if(getSelectedCardPath()===ancientsData[i].cardFace){
+        selectedCardIndex = i;
+        console.log(`selectedCardIndex:${selectedCardIndex}`);
+      }
+    }
+    console.log(`firststagebc: ${ancientsData[selectedCardIndex].firstStage.blueCards}`);
   })
 });
 function difficultyChoise () {
@@ -37,29 +43,36 @@ function getDifficulty() {
       secondPhase.style.display = 'block';
       difficultyChoise();
     }
-    console.log(selectedCardPath);
+
   });
   setTimeout(getDifficulty, 500)
 }
-function getSelectedCardPath(){
-  cards.forEach(element => {
-    // console.log(element.style.backgroundImage)
-    if (element.classList.contains('active')){
-      // console.log((element.style.backgroundImage));
-      selectedCardPath = element.style.backgroundImage.replace('url(', '').replace(')', '');
+
+
+function getSelectedCardPath () {
+  for (let card of cards){
+    if (card.classList.contains('active')){
+      var selectedCardPath = card.style.backgroundImage.replace('url(', '').replace(')', '').replace('"', '').replace('"', '');
       return selectedCardPath;
-      
     }
-  });
-  
-  setTimeout(getSelectedCardPath, 500)
+  }
 }
-getSelectedCardPath();
+
+function getFirstStageSet(){
+  let arr = [ancientsData[selectedCardIndex].firstStage.greenCards, ancientsData[selectedCardIndex].secondStage.greenCards, ancientsData[selectedCardIndex].thirdStage.greenCards];
+  document.querySelectorAll('.dot.green').forEach(element => {
+    for(let i=0;i<arr.length;i++){
+      element.textContent = arr[i];
+    }
+    
+  });
+
+}
+
+getFirstStageSet();
 getDifficulty();
 shuffle.addEventListener('click', ()=>{
   shuffle.style.display = 'none';
 })
-console.log(ancientsData[0].cardFace);
+// console.log(ancientsData[selectedCardIndex].cardFace);
 // lastCard.style.backgroundImage = `url(${ancientsData[0].cardFace})`;
-
-console.log(selectedCardPath);
