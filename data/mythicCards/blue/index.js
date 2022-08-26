4235523c500d20255de98c89788dd21c0079f6ed
MDViktor <milliondollarboymvd@gmail.com>
2022-08-26 15:35:03 +0300

    fix: modules pathes

--- data/mythicCards/blue/index.js
+++ data/mythicCards/blue/index.js
@@ -1,78 +1,78 @@
-import blueCardsAssets from '../../../assets/MythicCards/blue';
+import cards from '../../../assets/MythicCards/blue/index.js';
 
-const cardsData = [
+const cardsData1 = [
   {
     id: 'blue1',
-    cardFace: blueCardsAssets.blue1,
+    cardFace: cards.blue1,
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue2',
-    cardFace: blueCardsAssets.blue2,
+    cardFace: cards.blue2,
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue3',
-    cardFace: blueCardsAssets.blue3,
+    cardFace: cards.blue3,
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue4',
-    cardFace: blueCardsAssets.blue4,
+    cardFace: cards.blue4,
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue5',
-    cardFace: blueCardsAssets.blue5,
+    cardFace: cards.blue5,
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue6',
-    cardFace: blueCardsAssets.blue6,
+    cardFace: cards.blue6,
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue7',
-    cardFace: blueCardsAssets.blue7,
+    cardFace: cards.blue7,
     difficulty: 'normal',
     color:'blue'
   },
   {
     id: 'blue8',
-    cardFace: blueCardsAssets.blue8,
+    cardFace: cards.blue8,
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue9',
-    cardFace: blueCardsAssets.blue9,
+    cardFace: cards.blue9,
     difficulty: 'normal',
     color:'blue'
   },
   {
     id: 'blue10',
-    cardFace: blueCardsAssets.blue10,
+    cardFace: cards.blue10,
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue11',
-    cardFace: blueCardsAssets.blue11,
+    cardFace: cards.blue11,
     difficulty: 'normal',
     color:'blue'
   },
   {
     id: 'blue12',
-    cardFace: blueCardsAssets.blue12,
+    cardFace: cards.blue12,
     difficulty: 'normal',
     color:'blue'
   },
 ]
 
-export default cardsData
+export default cardsData1
--- data/mythicCards/green/index.js
+++ data/mythicCards/green/index.js
@@ -1,111 +1,111 @@
-import greenCardsAssets from '../../../assets/MythicCards/green/index.js';
+import card from '../../../assets/MythicCards/green/index.js';
 
 const cardsData = [
   {
     id: 'green1',
-    cardFace: greenCardsAssets.green1,
+    cardFace: card.green1,
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green2',
-    cardFace: greenCardsAssets.green2,
+    cardFace: card.green2,
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green3',
-    cardFace: greenCardsAssets.green3,
+    cardFace: card.green3,
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green4',
-    cardFace: greenCardsAssets.green4,
+    cardFace: card.green4,
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green5',
-    cardFace: greenCardsAssets.green5,
+    cardFace: card.green5,
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green6',
-    cardFace: greenCardsAssets.green6,
+    cardFace: card.green6,
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green7',
-    cardFace: greenCardsAssets.green7,
+    cardFace: card.green7,
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green8',
-    cardFace: greenCardsAssets.green8,
+    cardFace: card.green8,
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green9',
-    cardFace: greenCardsAssets.green9,
+    cardFace: card.green9,
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green10',
-    cardFace: greenCardsAssets.green10,
+    cardFace: card.green10,
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green11',
-    cardFace: greenCardsAssets.green11,
+    cardFace: card.green11,
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green12',
-    cardFace: greenCardsAssets.green12,
+    cardFace: card.green12,
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green13',
-    cardFace: greenCardsAssets.green13,
+    cardFace: card.green13,
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green14',
-    cardFace: greenCardsAssets.green14,
+    cardFace: card.green14,
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green15',
-    cardFace: greenCardsAssets.green15,
+    cardFace: card.green15,
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green16',
-    cardFace: greenCardsAssets.green16,
+    cardFace: card.green16,
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green17',
-    cardFace: greenCardsAssets.green17,
+    cardFace: card.green17,
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green18',
-    cardFace: greenCardsAssets.green18,
+    cardFace: card.green18,
     difficulty: 'easy',
     color:'green'
   },
